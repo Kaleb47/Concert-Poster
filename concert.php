3d2bba43c7f33333f066c5707ce710c750b5664c
ImJustJonny <113649337+ImJustJonny@users.noreply.github.com>
2023-11-30 11:10:54 -0500

    Update concert.php
    SQL still not linked. Ran into an issue. Checked and it all seemed correct. Perhaps I'm doing it wrong.

--- concert.php
+++ concert.php
@@ -1,53 +1,84 @@
 <?php
-// Declare and define global named constants
-define("TAX", 0.07);
-define("ADULT_COST", 36.75);
-define("CHILD_COST", 25.50);
-define("MIN_FEE", 0.50);
-define("MAX_FEE", 1.00);
-define("ATTEND", 5);
+// MariaDB connection configuration TO BE FIXED****
+$servername = "hermes.waketech.edu";
+$username = "jrussell8";
+$password = "csc124";
+$dbname = "Customers";
+$port = 3306;
+
+// Create a connection to the database using prepared statements
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 // Check if the form is submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve form data
-    $name = $_POST['userName']; // User's name
-    $email = $_POST['userPhone']; // User's phone number
-    $adultTickets = $_POST['adultTickets']; // Number of adult tickets
-    $childTickets = $_POST['childTickets']; // Number of child tickets
-    $date = $_POST['eventDate']; // Selected event date
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Retrieve and sanitize form data
+    $name = filter_input(INPUT_POST, 'userName', FILTER_SANITIZE_STRING);
+    $email = filter_input(INPUT_POST, 'userPhone', FILTER_SANITIZE_EMAIL);
+    $adultTickets = filter_input(INPUT_POST, 'adultTickets', FILTER_VALIDATE_INT);
+    $childTickets = filter_input(INPUT_POST, 'childTickets', FILTER_VALIDATE_INT);
+    $date = filter_input(INPUT_POST, 'eventDate', FILTER_SANITIZE_STRING);
 
-    // Total tickets calculation
-    $totalTickets = $adultTickets + $childTickets;
+    // Check if any input is invalid
+    if ($name !== null && $email !== null && $adultTickets !== false && $childTickets !== false && $date !== null) {
+        // Constants
+        define("TAX", 0.07);
+        define("ADULT_COST", 36.75);
+        define("CHILD_COST", 25.50);
+        define("MIN_FEE", 0.50);
+        define("MAX_FEE", 1.00);
+        define("ATTEND", 5);
 
-    // Fee calculation based on the number of tickets
-    if ($totalTickets <= ATTEND) {
-        $fee = $totalTickets * MAX_FEE;
+        // Total tickets calculation
+        $totalTickets = $adultTickets + $childTickets;
+
+        // Fee calculation based on the number of tickets
+        $fee = ($totalTickets <= ATTEND) ? $totalTickets * MAX_FEE : $totalTickets * MIN_FEE;
+
+        // Subtotal calculation
+        $subtotal = ($adultTickets * ADULT_COST) + ($childTickets * CHILD_COST);
+
+        // Sales tax calculation
+        $salesTax = $subtotal * TAX;
+
+        // Total cost calculation
+        $totalCost = $subtotal + $salesTax + $fee;
+
+        // SQL to insert data into the Customer table
+        $sql = "INSERT INTO Customer (LastName, FirstName, Email, AdultTicket, ChildTicket, Date, Location, Subtotal, Tax, Fee, TotalCost) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        
+        // Prepare and bind the statement
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("sssiisdddd", $name, $email, $adultTickets, $childTickets, $date, $location, $subtotal, $salesTax, $fee, $totalCost);
+        
+        // Execute the statement
+        $stmt->execute();
+        
+        // Close statement
+        $stmt->close();
+
+        // Display the results
+        echo "<p>Thank you <b>$name</b> at <b>$email</b>.<br/>";
+        echo "Details of your total cost <b>$" . number_format($totalCost, 2) . "</b> are shown below:</p>";
+        echo "<ul>";
+        echo "<li>Adult Tickets: $adultTickets</li>";
+        echo "<li>Child Tickets: $childTickets</li>";
+        echo "<li>Date: $date</li>";
+        echo "<li>Sub-total: $" . number_format($subtotal, 2) . "</li>";
+        echo "<li>Sales tax: $" . number_format($salesTax, 2) . "</li>";
+        echo "<li>Fee: $" . number_format($fee, 2) . "</li>";
+        echo "<li><b>TOTAL:</b> $" . number_format($totalCost, 2) . "</li>";
+        echo "</ul>";
+        echo "<h3>Thank you for using this program!</h3>";
     } else {
-        $fee = $totalTickets * MIN_FEE;
+        echo "Invalid input data!";
     }
-
-    // Subtotal calculation
-    $subtotal = ($adultTickets * ADULT_COST) + ($childTickets * CHILD_COST);
-    
-    // Sales tax calculation
-    $salesTax = $subtotal * TAX;
-    
-    // Total cost calculation
-    $totalCost = $subtotal + $salesTax + $fee;
-
-    // Display the results
-    echo "<p>Thank you <b>$name</b> at <b>$email</b>.<br/>";
-    echo "Details of your total cost <b>$" . number_format($totalCost, 2) . "</b> are shown below:</p>";
-    echo "<ul>";
-    echo "<li>Adult Tickets: $adultTickets</li>";
-    echo "<li>Child Tickets: $childTickets</li>";
-    echo "<li>Date: $date</li>";
-    // The location should be added here if you have that data.
-    echo "<li>Sub-total: $" . number_format($subtotal, 2) . "</li>";
-    echo "<li>Sales tax: $" . number_format($salesTax, 2) . "</li>";
-    echo "<li>Fee: $" . number_format($fee, 2) . "</li>";
-    echo "<li><b>TOTAL:</b> $" . number_format($totalCost, 2) . "</li>";
-    echo "</ul>";
-    echo "<h3>Thank you for using this program!</h3>";
 }
+
+// Close the connection
+$conn->close();
 ?>
